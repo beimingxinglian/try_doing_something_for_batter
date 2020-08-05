44dee561cae6b85a986515d0fb4cd2031a491d0f
BeiMingXingLian <beimingxinglian@gmail.com>
2020-08-05 08:12:22 +0800

    实现滚动条布局

--- collective_docker/box_UI.py
+++ collective_docker/box_UI.py
@@ -2,36 +2,61 @@
 from PySide2 import QtCore, QtGui, QtWidgets
 
 from func_library import frequency_func
+from ui import description
+
+reload(frequency_func)
+reload(description)
+
+
+class DescriptionWidget(QtWidgets.QWidget, description.Ui_Form):
+    def __init__(self):
+        super(DescriptionWidget, self).__init__()
+        self.setupUi(self)
 
 
 class UiMainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         self.maya_mainwindow = frequency_func.maya_mainwindow()
+        self.description_widget = DescriptionWidget()
         super(UiMainWindow, self).__init__(self.maya_mainwindow)
         self.setupUi()
 
     def setupUi(self):
         self.setObjectName("box_window")
         self.setWindowTitle(u'羊神盒子仿制版')
-        self.resize(500, 800)
+        self.resize(300, 800)
 
         self.centralwidget = QtWidgets.QWidget(self)
         self.setCentralWidget(self.centralwidget)
         self.window_layout = QtWidgets.QVBoxLayout(self.centralwidget)
         self.setLayout(self.window_layout)
 
+        # 创建一个可调整子控件大小的widget
         splitter2 = QtWidgets.QSplitter(QtCore.Qt.Vertical)
+
         self.window_layout.addWidget(splitter2)
 
-        # todo：上下分割，布局随窗口缩放， 动态增加按钮
-        self.bottom = QtWidgets.QFrame()
-        self.bottom.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        splitter2.addWidget(self.bottom)
+        # 创建一个包含滚动条的widget
+        self.scroll_widget = QtWidgets.QScrollArea()
+        self.vbox_scroll = QtWidgets.QVBoxLayout()
+        self.scroll_widget.setLayout(self.vbox_scroll)
 
-        textedit = QtWidgets.QTextEdit()
-        splitter2.addWidget(textedit)
+        self.button_widget = QtWidgets.QWidget()
+        self.button_widget.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        # button的大小需要锁定，
+        self.button_widget.setMinimumSize(300, 4000)
 
-        label = QtWidgets.QLabel()
-        label.setFrameStyle(QtWidgets.QFrame.Panel | QtWidgets.QFrame.Raised)
-        label.setLineWidth(2)
-        splitter2.addWidget(label)
+        self.scroll_widget.setWidget(self.button_widget)
+
+        self.vbox = QtWidgets.QVBoxLayout()
+        self.button_widget.setLayout(self.vbox)
+
+        splitter2.addWidget(self.scroll_widget)
+        splitter2.addWidget(self.description_widget)
+
+        # 增加按钮
+        for i in range(30):
+            button = QtWidgets.QPushButton()
+            button.setText('PushButton{}'.format(i))
+            button.setObjectName('PushButton{}'.format(i))
+            self.vbox.addWidget(button)
---
+++ collective_docker/ui/description.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'description.ui'
+#
+# Created: Tue Aug  4 22:17:51 2020
+#      by: pyside2-uic  running on PySide2 2.0.0~alpha0
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide2 import QtCore, QtGui, QtWidgets
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(257, 202)
+        self.verticalLayout_2 = QtWidgets.QVBoxLayout(Form)
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.pushButton_2 = QtWidgets.QPushButton(Form)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.horizontalLayout.addWidget(self.pushButton_2)
+        self.pushButton_3 = QtWidgets.QPushButton(Form)
+        self.pushButton_3.setObjectName("pushButton_3")
+        self.horizontalLayout.addWidget(self.pushButton_3)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout.addWidget(self.pushButton)
+        self.verticalLayout_2.addLayout(self.horizontalLayout)
+        self.textEdit = QtWidgets.QTextEdit(Form)
+        self.textEdit.setReadOnly(True)
+        self.textEdit.setObjectName("textEdit")
+        self.verticalLayout_2.addWidget(self.textEdit)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QtWidgets.QApplication.translate("Form", "Form", None, -1))
+        self.pushButton_2.setText(QtWidgets.QApplication.translate("Form", "PushButton", None, -1))
+        self.pushButton_3.setText(QtWidgets.QApplication.translate("Form", "PushButton", None, -1))
+        self.pushButton.setText(QtWidgets.QApplication.translate("Form", "PushButton", None, -1))
+
